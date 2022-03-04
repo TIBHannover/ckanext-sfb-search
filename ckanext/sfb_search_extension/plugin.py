11eed72ab1447fc239b73b828e5d8900019fd32c
pooya.oa <pooya.oladazimi@tib.eu>
2022-03-04 11:21:20 +0100

    revert back to IResourceController

--- ckanext/sfb_search_extension/plugin.py
+++ ckanext/sfb_search_extension/plugin.py
@@ -5,7 +5,8 @@ from ckanext.sfb_search_extension.libs.helpers import Helper
 
 class AutoTagPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IPackageController)
+    plugins.implements(plugins.IResourceController)
+
 
     # IConfigurer
 
@@ -14,69 +15,57 @@ class AutoTagPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         
 
-    
-    # IPackageController
 
+     # IResourceController
 
-    def after_create(self, context, pkg_dict):
-        for res in pkg_dict['resources']:
-            if res['url_type'] == 'upload':
-                try:
-                    dataframe_columns = []
-                    xls_dataframes_columns = {}
-                    if Helper.is_csv(res):                        
-                        dataframe_columns = Helper.get_csv_columns(res['id'])
-                        for col in dataframe_columns:
+    def after_create(self, context, resource):
+        if resource['url_type'] == 'upload':
+            try:
+                dataframe_columns = []
+                xls_dataframes_columns = {}
+                if Helper.is_csv(res):                        
+                    dataframe_columns = Helper.get_csv_columns(resource['id'])
+                    for col in dataframe_columns:
+                        if col not in pkg_dict['tags']:
+                            pkg_dict['tags'].append(col)
+                    
+                elif Helper.is_xlsx(res):
+                    xls_dataframes_columns = Helper.get_xlsx_columns(resource['id'])
+                    for sheet, columns in xls_dataframes_columns.items():
+                        for col in columns:
                             if col not in pkg_dict['tags']:
                                 pkg_dict['tags'].append(col)
-                        
-                    elif Helper.is_xlsx(res):
-                        xls_dataframes_columns = Helper.get_xlsx_columns(res['id'])
-                        for sheet, columns in xls_dataframes_columns.items():
-                            for col in columns:
-                                if col not in pkg_dict['tags']:
-                                    pkg_dict['tags'].append(col)
-                    else:
-                        continue
-                
-                except:
-                    # continue
-                    raise
+                else:
+                    continue
+            
+            except:
+                # continue
+                raise
   
-        return pkg_dict
+        return resource
 
 
-    def read(self, entity):
-        return entity
+    
+    def before_create(self, context, resource):
+        return resource
 
-    def create(self, entity):
-        return entity
+    def before_update(self, context, current, resource):
+        return resource
     
-    def edit(self, entity):
-        return entity
-    def delete(self, entity):
-        return entity
+    def after_update(self, context, resource):
+        return resource
     
-    def after_update(self, context, pkg_dict):
-        return pkg_dict
+    def before_delete(self, context, resource, resources):
+        return resources
     
-    def after_delete(self, context, pkg_dict):
-        return pkg_dict
+    def after_delete(self, context, resources):
+        return resources
     
     def before_show(self, resource_dict):
         return resource_dict
 
-    def after_show(self, pkg_dict):
-        return pkg_dict
+
+
+
     
-    def before_search(self, search_params):
-        return search_params
-    
-    def after_search(self, search_results, search_params):
-        return search_results
-    
-    def before_index(self, pkg_dict):
-        return pkg_dict
-    
-    def before_view(self, pkg_dict):
-        return pkg_dict
+ 
