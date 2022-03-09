9b30337774b0ecb915f9400bf40793ff0304339e
pooya.oa <pooya.oladazimi@tib.eu>
2022-03-09 11:46:59 +0100

    resource_column_search: implmented IPackageController

--- ckanext/sfb_search_extension/plugin2.py
+++ ckanext/sfb_search_extension/plugin2.py
@@ -6,6 +6,7 @@ from ckanext.sfb_search_extension.libs.helpers import Helper
 
 class ResourceColumnSearchPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IPackageController)
 
 
     # IConfigurer
@@ -13,3 +14,42 @@ class ResourceColumnSearchPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
+    
+
+    # IPackageController
+
+    def read(self, entity):
+        pass
+
+    def create(self, entity):
+        pass
+
+    def edit(self, entity):
+        pass
+
+    def delete(self, entity):
+        pass
+
+    def after_create(self, context, pkg_dict):
+        pass
+
+    def after_update(self, context, pkg_dict):
+        pass
+
+    def after_delete(self, context, pkg_dict):
+        pass
+
+    def after_show(self, context, pkg_dict):
+        pass
+
+    def before_search(self, search_params):
+        pass
+
+    def after_search(self, search_results, search_params):
+        pass
+
+    def before_index(self, pkg_dict):
+        pass
+
+    def before_view(self, pkg_dict):
+        pass 
