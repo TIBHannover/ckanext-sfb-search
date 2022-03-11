a1c4b45414601db09c52648d90c63ab749b46b95
pooya.oa <pooya.oladazimi@tib.eu>
2022-03-11 12:07:29 +0100

    pass the detected resource id to template

--- ckanext/sfb_search_extension/plugin2.py
+++ ckanext/sfb_search_extension/plugin2.py
@@ -32,7 +32,7 @@ class ResourceColumnSearchPlugin(plugins.SingletonPlugin):
         # empty the search result to remove unrelated search result by ckan.
         search_results['results'] = [] 
         search_results['count'] = 0
-        print(search_results['facets']) 
+        search_results['detected_resources_ids'] = []
         search_filters = search_params['fq'][0]
         all_datasets = Package.search_by_name('')
         for package in all_datasets:
@@ -67,13 +67,11 @@ class ResourceColumnSearchPlugin(plugins.SingletonPlugin):
                             search_results['search_facets'] = Helper.update_search_facet(search_results['search_facets'], dataset, 'organization')
                             search_results['search_facets'] = Helper.update_search_facet(search_results['search_facets'], dataset, 'tags')
                             search_results['search_facets'] = Helper.update_search_facet(search_results['search_facets'], dataset, 'groups')
-                            search_results = Helper.add_search_result(dataset, search_filters, search_results)                            
+                            if not detected:
+                                search_results = Helper.add_search_result(dataset, search_filters, search_results)                            
                             detected = True
+                            search_results['detected_resources_ids'].append(res['id'])
                             break
-                    
-                    if detected:
-                        break
-
                 
                 elif Helper.is_xlsx(res):
                     # resource is excel sheet
@@ -89,25 +87,25 @@ class ResourceColumnSearchPlugin(plugins.SingletonPlugin):
                                 col_name = str(col_name)
                             except:
                                 continue
-                            
+
                             if search_phrase in col_name.strip().lower():
                                 search_results['search_facets'] = Helper.update_search_facet(search_results['search_facets'], dataset, 'sfb_dataset_type')
                                 search_results['search_facets'] = Helper.update_search_facet(search_results['search_facets'], dataset, 'organization')
                                 search_results['search_facets'] = Helper.update_search_facet(search_results['search_facets'], dataset, 'tags')
                                 search_results['search_facets'] = Helper.update_search_facet(search_results['search_facets'], dataset, 'groups')
-                                search_results = Helper.add_search_result(dataset, search_filters, search_results) 
+                                if not detected:
+                                    search_results = Helper.add_search_result(dataset, search_filters, search_results)
+                                search_results['detected_resources_ids'].append(res['id']) 
                                 detected = True
                                 break
                         
                         if detected:
                             break
-                    
-                    if detected:
-                        break
-                
+                                    
                 else:
                     continue
         
+        toolkit.g.detected_resources_ids = search_results['detected_resources_ids']
         return search_results
 
 
