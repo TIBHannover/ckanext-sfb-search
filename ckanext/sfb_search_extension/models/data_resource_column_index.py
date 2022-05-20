a3f4e951cb28505ddbbdef4440f5a49eb29d8144
pooya.oa <pooya.oladazimi@tib.eu>
2022-05-20 10:41:03 +0200

    create the index table model

---
+++ ckanext/sfb_search_extension/models/data_resource_column_index.py
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+from sqlalchemy import Column, Table, ForeignKey, orm
+from sqlalchemy import types as _types
+from sqlalchemy.sql.expression import false
+from ckan.model import meta, Resource, domain_object
+
+
+__all__ = [u"DataResourceColumnIndex", u"data_resource_column_index_table"]
+
+data_resource_column_index_table = Table(
+    u"data_resource_column_index",
+    meta.metadata,
+    Column(u"id", _types.Integer, primary_key=True, nullable=False),
+    Column(u"resource_id", _types.UnicodeText, ForeignKey(u"resource.id"), nullable=False),
+    Column(u"columns_names", _types.UnicodeText, nullable=False)
+)
+
+class DataResourceColumnIndex(domain_object.DomainObject):
+    def __init__(self, resource_id=None, columns_names=None):
+        self.resource_id = resource_id
+        self.columns_names = columns_names        
+     
+
+    @classmethod
+    def get_by_resource(cls, id, autoflush=True):
+        if not id:
+            return None
+
+        exists = meta.Session.query(cls).filter(cls.resource_id==id).first() is not None
+        if not exists:
+            return False
+        query = meta.Session.query(cls).filter(cls.resource_id==id)
+        query = query.autoflush(autoflush)
+        record = query
+        return record
+
+    
+    def get_resource(self):
+        return self.resource
+
+
+
+meta.mapper(
+    DataResourceColumnIndex,
+    data_resource_column_index_table,
+    properties={
+        u"resource": orm.relation(
+            Resource, backref=orm.backref(u"data_resource_column_index", cascade=u"all, delete, delete-orphan")
+        )
+    },
+)
