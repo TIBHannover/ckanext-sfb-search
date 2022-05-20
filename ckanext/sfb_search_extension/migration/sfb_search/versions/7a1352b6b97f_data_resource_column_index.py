411080dbe37218a50cfdf86d73edd28c9a38f245
pooya.oa <pooya.oladazimi@tib.eu>
2022-05-20 10:09:03 +0200

    created migration for resource column index

---
+++ ckanext/sfb_search_extension/migration/sfb_search/versions/7a1352b6b97f_data_resource_column_index.py
@@ -0,0 +1,24 @@
+"""data_resource_column_index
+
+Revision ID: 7a1352b6b97f
+Revises: 
+Create Date: 2022-05-20 10:08:12.920497
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7a1352b6b97f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
