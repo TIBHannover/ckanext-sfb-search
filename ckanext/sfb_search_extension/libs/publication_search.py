e97374b3c812282664730c237cb421f9e41b32bd
pooya.oa <pooya.oladazimi@tib.eu>
2022-10-06 11:03:51 +0200

    increase the similarity precision with Tfidf

--- ckanext/sfb_search_extension/libs/publication_search.py
+++ ckanext/sfb_search_extension/libs/publication_search.py
@@ -1,9 +1,11 @@
 # encoding: utf-8
 
+from this import d
 import ckan.plugins.toolkit as toolkit
 from ckanext.sfb_search_extension.libs.commons import CommonHelper
 from sqlalchemy.sql.expression import false
 import re
+from sklearn.feature_extraction.text import TfidfVectorizer
 if CommonHelper.check_plugin_enabled("dataset_reference"):
     from ckanext.dataset_reference.models.package_reference_link import PackageReferenceLink
 
@@ -59,10 +61,10 @@ class PublicationSearchHelper():
             if linked_publications == false:
                 continue
 
-            for pub in linked_publications:
+            for pub in linked_publications:                
                 if not pub.citation:
-                    continue                
-                elif re.search(search_phrase, pub.citation, re.IGNORECASE):                    
+                    continue                                
+                elif PublicationSearchHelper.similarity_calc(search_phrase.lower(), pub.citation.lower()) >= 0.7:
                     if not detected:
                         search_results['search_facets'] = CommonHelper.update_search_facet(search_results['search_facets'], dataset, 'sfb_dataset_type')
                         search_results['search_facets'] = CommonHelper.update_search_facet(search_results['search_facets'], dataset, 'organization')
@@ -70,7 +72,7 @@ class PublicationSearchHelper():
                         search_results['search_facets'] = CommonHelper.update_search_facet(search_results['search_facets'], dataset, 'groups')
                         search_results = CommonHelper.add_search_result(dataset, search_filters, search_results)
                     detected = True 
-                else:
+                elif search_phrase.lower() in pub.citation.lower():
                     tokens = search_phrase.split(' ')
                     for tok in tokens:
                         if tok.lower() in pub.citation.lower():
@@ -86,6 +88,36 @@ class PublicationSearchHelper():
         return search_results
 
     
+
+    @staticmethod
+    def similarity_calc(query, doc):
+        '''
+            Calculate the similarity between the search input and the target doc.
+
+            Args:
+                - query: Input search query
+                - doc: the target doc
+            
+            Return:
+                - The similartiy score
+        '''
+
+        corpus = [query, doc]
+        vectorModel = TfidfVectorizer(min_df=1)
+        tfidf = vectorModel.fit_transform(corpus)
+        similarities = tfidf * tfidf.T
+        print("##################################################################")
+        print("##################################################################")
+        print("##################################################################")
+        print("############################################vv######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print(similarities.toarray())
+        return float(similarities.toarray()[0][1])
+
     
 
-   
+
+    
